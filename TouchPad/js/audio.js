b42cee223b0fecad0e510a2c3b0c31580bc85e4b
Alexander Fang <alex.s.fang@gmail.com>
2016-04-09 15:42:01 -0500

    Play trumpet sounds and blues background
    moved trumpet sounds to under project workspace

--- TouchPad/js/audio.js
+++ TouchPad/js/audio.js
@@ -1,3 +1,56 @@
+var sounds = {
+	//C Mixolydian scale; bottom row of letters (and comma) (zxcvbnm,)
+	90: '60', // z is C4
+	88: '62', // x is D4
+	67: '64', // c is E4
+	86: '65', // v is F4
+	66: '67', // b is G4
+	78: '69', // n is A4
+	77: '70', // m is Bb4
+	188: '72', // , is C5
+
+	//F Mixolydian scale; middle of row of letters (asdfghjk)
+	65: '65', // a is F4
+	83: '67', // s is G4
+	68: '69', // d is A4
+	70: '70', // f is Bb4
+	71: '72', // g is C5
+	72: '74', // h is D5
+	74: '75', // j is Eb5
+	75: '77', // k is F5
+
+	//G Mixolydian scale, top of row of letters (qwertyui)
+	81: '67', // q is G4
+	87: '69', // w is A4
+	69: '71', // e is B4
+	82: '72', // r is C5
+	84: '74', // t is D5
+	89: '76', // y is E5
+	85: '77', // u is F5
+	73: '79' // i is G5
+}
+
+
+//play background 12-bar blues
+var audio = new Audio('./sounds/blues.wav');
+audio.play();
+
+
+document.onkeydown = function(e) {
+    var soundId = sounds[e.keyCode];
+    if (soundId) document.getElementById(soundId).play();
+    else console.log("key not mapped : code is", e.keyCode);
+}
+
+document.onkeyup = function(e) {
+    var soundId = sounds[e.keyCode];
+    if (soundId) {document.getElementById(soundId).pause();
+    	document.getElementById(soundId).currentTime = 0}
+    else console.log("key not mapped : code is", e.keyCode);
+}
+
+
+
 function SoundEvent( e ) {
   this.pointerId = e.pointerId;
   this.x = e.clientX;
